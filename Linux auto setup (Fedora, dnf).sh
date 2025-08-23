b4ddfa36b8a15b64a7be9069b551fb0347a02e58
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2025-08-23 17:33:41 +0200

    Added bitwarden desktop flatpak

--- Linux auto setup (Fedora, dnf).sh
+++ Linux auto setup (Fedora, dnf).sh
@@ -247,6 +247,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     flatpak install flathub io.missioncenter.MissionCenter -y
     flatpak install flathub io.github.nate_xyz.Resonance -y
     flatpak install flathub org.musicbrainz.Picard -y
+    flatpak install flathub com.bitwarden.desktop -y
     
     #Steam and Proton GE
     flatpak install flathub com.valvesoftware.Steam -y
