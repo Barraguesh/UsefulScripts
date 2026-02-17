08282e0cb9cae20923eaef9968d5140e18da3a5e
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2026-02-17 20:46:11 +0100

    Changed favorite apps

--- Linux auto setup (Fedora Silverblue, rpm-ostree).sh
+++ Linux auto setup (Fedora Silverblue, rpm-ostree).sh
@@ -260,7 +260,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     dconf write /org/gnome/Ptyxis/default-rows "uint32 25"
     
     #Dock personalization (gsettings get org.gnome.shell favorite-apps)
-    gsettings set org.gnome.shell favorite-apps "['org.gnome.Ptyxis.desktop', 'org.gnome.Nautilus.desktop', 'org.mozilla.firefox.desktop', 'com.bitwarden.desktop.desktop', 'md.obsidian.Obsidian.desktop', 'com.valvesoftware.Steam.desktop', 'org.gnome.Music.desktop', 'com.stremio.Stremio.desktop', 'com.visualstudio.code.desktop']"
+    gsettings set org.gnome.shell favorite-apps "['org.gnome.Ptyxis.desktop', 'org.gnome.Nautilus.desktop', 'org.mozilla.firefox.desktop', 'com.bitwarden.desktop.desktop', 'md.obsidian.Obsidian.desktop', 'com.valvesoftware.Steam.desktop', 'app.xemu.xemu.desktop', 'com.nvidia.geforcenow.desktop', 'org.gnome.Music.desktop', 'com.stremio.Stremio.desktop', 'com.visualstudio.code.desktop', 'org.cryptomator.Cryptomator.desktop']"
 
     #Extensions config
     dconf write /org/gnome/shell/extensions/just-perfection/window-demands-attention-focus true
