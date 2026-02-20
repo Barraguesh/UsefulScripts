64573f9d8a8d3e5eab0e8c22eede859edfcab318
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2026-02-20 20:12:04 +0100

    Change rpm fusion install per documentation

--- Linux auto setup (Fedora Silverblue, rpm-ostree).sh
+++ Linux auto setup (Fedora Silverblue, rpm-ostree).sh
@@ -62,9 +62,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo flatpak remote-modify --enable flathub
 
     #RPM fusion repo
-    rpm-ostree install \
-      https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-      https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     
     sudo systemctl reboot
 fi
